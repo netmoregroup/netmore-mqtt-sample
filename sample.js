ece7f680e81e8219e6b8447c5f1ef0f0ae2de25a
Jimmy Björklund <9413089+JimmyBjorklund@users.noreply.github.com>
2023-02-02 15:33:25 +0100

    Take cert id from input and use it as part of path

--- sample.js
+++ sample.js
@@ -2,17 +2,18 @@
 var mqtt = require('mqtt');
 var fs = require('fs');
 
+const id = process.argv[2];
+
 const url = "mqtts://mqtt.netmoregroup.com";
-const ca = "./certs/952/ca.crt";
-const cert = "./certs/952/client.crt";
-const key = "./certs/952/client.key";
+const ca = `./certs/${id}/ca.crt`;
+const cert = `./certs/${id}/client.crt`;
+const key = `./certs/${id}/client.key`;
 
 if( process.argv.length !== 3 ) {
   console.error(`Id from cert not given! use ca name as is. openssl x509 -in ${cert} -noout -text`);
   console.log(`Note that the id need to match the cert common name, if not you will not recive any data.`);
   process.exit(1);
 }
-const id = process.argv[2];
 const fkey = fs.readFileSync(key, "utf8");
 const fcert = fs.readFileSync(cert, "utf8");
 const fca =  fs.readFileSync(ca, "utf8");
